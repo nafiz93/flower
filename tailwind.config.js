1fcdec68906dce8b97df1394f1858e19bd32a53f
nafiz93 <nafiz93@gmail.com>
2025-07-24 15:48:14 +0200

    card added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
-            fontFamily: {
-                inter: ['Inter', 'sans-serif'],
-                noto: ['"Noto Serif"', 'serif'],
-            },
+
         },
     },
     plugins: [],
